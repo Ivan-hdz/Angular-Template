8d2d61ab85bd4b0e07e08e1c591ab1ad9ffff8b1
Iván Hernández <honter1997@gmail.com>
2020-07-28 11:36:36 -0500

    Update shared-material.module.ts

--- src/app/shared/modules/shared-material/shared-material.module.ts
+++ src/app/shared/modules/shared-material/shared-material.module.ts
@@ -3,25 +3,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatExpansionModule } from '@angular/material/expansion';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import {MatPaginatorIntl, MatPaginatorModule} from '@angular/material/paginator';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import {MatRadioModule} from '@angular/material/radio';
+import {MatPaginatorIntl} from '@angular/material/paginator';
 import {MatDividerModule} from '@angular/material/divider';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatListModule} from '@angular/material/list';
-import {MatStepperModule} from '@angular/material/stepper';
+import {LayoutModule} from "@angular/cdk/layout";
 
 const config = new MatPaginatorIntl();
 config.itemsPerPageLabel = 'Elementos por página';
@@ -34,9 +20,14 @@ config.previousPageLabel = 'Página anterior';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
   ],
-  exports: [ ],
+  exports: [
+    MatButtonModule,
+    LayoutModule,
+    MatDividerModule,
+    MatIconModule
+  ],
   providers: [
     {provide: MAT_DATE_LOCALE, useValue: 'es-MX'},
     {provide: MatPaginatorIntl, useValue: 'es-MX'},

04fc3291e9d0a2bc041b306b27d23429738e39d8
Iván Hernández <honter1997@gmail.com>
2020-07-28 11:35:24 -0500

    Update menu.component.ts

--- src/app/shared/modules/menu/components/menu/menu.component.ts
+++ src/app/shared/modules/menu/components/menu/menu.component.ts
@@ -1,19 +1,61 @@
 
 
-import {Component, OnInit} from '@angular/core';
-
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {RouterService} from "../../../routing/services/router.service";
+import {BreakpointObserver, BreakpointState} from "@angular/cdk/layout";
+import {MatDrawer} from "@angular/material/sidenav";
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  @ViewChild('drawer', {static: true}) drawer: MatDrawer;
+  public mode;
+  public shouldHaveMenu = false;
+  public tieneMuchosPerfiles: boolean;
+  public puedeCambiarPerfil: boolean;
+  constructor(public breakpointObserver: BreakpointObserver,
+              public router: RouterService) {
+    this.mode = 'side';
+    this.tieneMuchosPerfiles = false;
+    this.puedeCambiarPerfil = false;
+  }
+  private ponerResponsive() {
+    // css breakpoint
+    this.breakpointObserver
+      .observe(['(min-width: 720px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.mode = 'side';
+          if (this.shouldHaveMenu) {
+            this.drawer.open();
+          }
+        } else {
+          this.mode = 'over';
+          this.drawer.close();
+        }
+      });
+  }
 
-  constructor() {
- }
-
+  private alternarMenuSegunModoResponsive() {
+    if (this.mode === 'over') {
+      this.drawer.close();
+    } else {
+      this.drawer.open();
+    }
+    this.shouldHaveMenu = true;
+  }
+  private deshabilitarMenu() {
+    this.tieneMuchosPerfiles = false;
+    this.drawer.close();
+    this.shouldHaveMenu = false;
+  }
   ngOnInit() {
+    // open or close when Usuario is logged in/out
 
+    this.ponerResponsive();
+    this.alternarMenuSegunModoResponsive();
   }
 
 }
